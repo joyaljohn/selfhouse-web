422170dafe765ee27170d01179b245cc486ad149
Josh Burns <contact@jbernsie.xyz>
2019-06-19 07:43:37 +1000

    creation specs for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_042521) do
+ActiveRecord::Schema.define(version: 2019_06_18_212033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "publication_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publication_sub_categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "publication_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["publication_category_id"], name: "index_publication_sub_categories_on_publication_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +44,5 @@ ActiveRecord::Schema.define(version: 2019_06_18_042521) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "publication_sub_categories", "publication_categories"
 end
--- spec/models/publication_category_spec.rb
+++ spec/models/publication_category_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PublicationCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create" do
+    context "with all valid params" do
+      it "should save and return true" do
+        cat = PublicationCategory.new(attributes_for(:publication_category))
+        expect(cat.save).to eq(true)
+      end
+    end
+
+
+    context "missing params" do
+      it "should not save and return false" do
+        cat = PublicationCategory.new(attributes_for(:publication_category, name: nil))
+        expect(cat.save).to eq(false)
+      end
+    end
+  end
 end
