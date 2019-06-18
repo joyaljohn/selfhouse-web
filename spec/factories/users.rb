a7a1f4d541a1d79cb43c360149265caefac5e215
Josh Burns <contact@jbernsie.xyz>
2019-06-18 23:00:28 +1000

    user tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
-    email "test@selfhouse.local"    
-    first_name "Test"
-    last_name "User"
-    bio "Thi is a test user from the user factory"
-    password "password123"
-    password_confirmation "password123"
+    email { "test@selfhouse.local" }
+    first_name { "Test" }
+    last_name { "User" }
+    bio { "This is a test user from the user factory" }
+    password { "password123" }
+    password_confirmation { "password123" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create" do  
+    context "with all valid params" do
+      it "should save and return true" do
+        user = User.new(attributes_for(:user))
+        expect(user.save).to eq(true)
+      end
+    end
+
+    context "missing first name parameter" do
+      it "should not save and return false" do
+        user = User.new(attributes_for(:user, first_name: nil))
+        expect(user.save).to eq(false)
+      end
+    end
+
+    context "missing last name parameter" do
+      it "should not save and return false" do
+        user = User.new(attributes_for(:user, last_name: nil))
+        expect(user.save).to eq(false)
+      end
+    end
+
+    context "missing email parameter" do
+      it "should not save and return false" do
+        user = User.new(attributes_for(:user, email: nil))
+        expect(user.save).to eq(false)
+      end
+    end
+
+  end
 end
