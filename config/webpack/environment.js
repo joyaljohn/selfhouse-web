57579c4ba10f7c9de61ef1b47597446a6fa7ebaa
Josh Burns <contact@jbernsie.xyz>
2019-06-20 12:58:31 +1000

    bootstrap implemented styled navbar with dynamic user menu

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,7 +3,7 @@ const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const webpack = require('webpack')
 
-environment.plugins.set('Provide', new webpack.ProvidePlugin({
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
