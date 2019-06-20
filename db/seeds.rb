482288bab64ad5f9f8543fa1f3c95fbb0fcda6eb
Josh Burns <contact@jbernsie.xyz>
2019-06-21 08:08:53 +1000

    Development Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,31 @@ User.create(first_name: "Test",
             email: "test@test.com",
             password: "password",
             password_confirmation: "password")
+
+# Parent Categories
+PublicationCategory.create([
+  { name: "Fiction/Novels" },
+  { name: "Non-Fiction" },
+  { name: "Academia" }
+])
+
+# Sub-Categories for Fiction
+PublicationSubCategory.create([
+  {name: "1", publication_category: PublicationCategory.where(name: "Fiction/Novels").first},
+  {name: "2", publication_category: PublicationCategory.where(name: "Fiction/Novels").first},
+  {name: "3", publication_category: PublicationCategory.where(name: "Fiction/Novels").first}
+])
+
+# Sub-Categories for Non-Fiction
+PublicationSubCategory.create([
+  {name: "1", publication_category: PublicationCategory.where(name: "Non-Fiction").first},
+  {name: "2", publication_category: PublicationCategory.where(name: "Non-Fiction").first},
+  {name: "3", publication_category: PublicationCategory.where(name: "Non-Fiction").first}
+])
+
+# Sub-Categories for Academia
+PublicationSubCategory.create([
+  {name: "1", publication_category: PublicationCategory.where(name: "Academia").first},
+  {name: "2", publication_category: PublicationCategory.where(name: "Academia").first},
+  {name: "3", publication_category: PublicationCategory.where(name: "Academia").first}
+])
