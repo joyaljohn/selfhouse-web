277bcc96952306c503a3441ce9fc619c803f1f06
Josh Burns <contact@jbernsie.xyz>
2019-06-22 21:39:08 +1000

    display a list of the latest publications on the Homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class StaticController < ApplicationController
-  def home; end
+  def home
+    @newpubs = Publication.order(created_at: :asc).last(5)
+  end
 
   def landing; end
 end
