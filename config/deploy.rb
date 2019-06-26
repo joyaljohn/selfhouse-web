cc141f9c810b9ac1926cb8470333e9ddb3f56df7
Josh Burns <contact@jbernsie.xyz>
2019-06-26 11:14:37 +1000

    deployment configurations conform to styleguide

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,13 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+# frozen_string_literal: true
 
-set :application, "selfhouse"
-set :repo_url, "git@github.com:selfhouse/selfhouse-web.git"
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.11.0'
+
+set :application, 'selfhouse'
+set :repo_url, 'git@github.com:selfhouse/selfhouse-web.git'
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", ".bundle", "storage"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle', '.bundle', 'storage'
 
 set :keep_releases, 5
